8e1e189e535d9da1e416875c5cac23ae03e58c2a
Karalee Shotola <karalee@utexas.edu>
2018-07-05 11:47:08 +0200

    Added aria roles and labels

--- js/main.js
+++ js/main.js
@@ -178,6 +178,7 @@ createRestaurantHTML = (restaurant) => {
 
   const more = document.createElement('button');
   more.innerHTML = 'View Details';
+  more.setAttribute('aria-label', 'View Details of ' + restaurant.name);
   more.onclick = function() {
     const url = DBHelper.urlForRestaurant(restaurant);
     window.location = url;
