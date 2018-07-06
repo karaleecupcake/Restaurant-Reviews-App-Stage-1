d294a725ad4b2ad937569a4b4a7b0bcaef2f0b8f
Karalee Shotola <karalee@utexas.edu>
2018-07-06 12:04:38 +0200

    Added service worker registration

--- js/main.js
+++ js/main.js
@@ -217,14 +217,9 @@ addMarkersToMap = (restaurants = self.restaurants) => {
 
 /* Sets up service worker */
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-  .register("./sw.js", {
-    scope: ""
-  })
-  .then(function(reg) {
-    console.log("Service worker registered " + reg.scope);
-  })
-  .catch(function(error) {
-    console.log("Registration failed " + error);
+  navigator.serviceWorker.register("./sw.js").then(function(reg) {
+    console.log("Service worker registered");
+  }).catch(function(error) {
+    console.log("Service worker registration failed " + error);
   });
 }
